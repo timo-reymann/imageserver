79930c618c865d064bc1faeec1cf70650fa0d89c
Timo Reymann <mail@timo-reymann.de>
2018-10-07 22:08:48 +0200

    Switch to buidl script instead of jenkinsfile
    The docker image wont build on Jenkins, so i switched backed to a plain old shell script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -t timoreymann/imageserver:latest .
+docker push timoreymann/imageserver:latest
+
+docker build -t timoreymann/imageserver:$1
+docker push timoreymann/imageserver:$1
