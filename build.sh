8ecb79da1b563a213cc8dfe6ebed2b5e0bf7b876
Timo Reymann <mail@timo-reymann.de>
2018-10-08 20:12:15 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 docker build -t timoreymann/imageserver:latest .
 docker push timoreymann/imageserver:latest
 
-docker build -t timoreymann/imageserver:$1
+docker build -t timoreymann/imageserver:$1 .
 docker push timoreymann/imageserver:$1
